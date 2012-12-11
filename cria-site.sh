6514f82d9ac2067e463bc0d3c193ea81c114e26e
Lucas Mezêncio <lucas.mezencio@gmail.com>
2012-12-11 10:28:11 -0200

    Start the english translation

--- cria-site.sh
+++ cria-site.sh
@@ -1,82 +1,81 @@
 #!/bin/bash
 
-# Verificando se o usuario e root ou esta usando sudo
+# Check if the user is logged as root or using sudo
 if [ "$(whoami)" != "root" ]; then
-    echo "Voce nao tem permissoes para rodar \"$0\" com um usuario comum. Use sudo!"
+    echo "You don't have enough permissions to run \"$0\" as a normal user. Use sudo or root!"
     exit 1
 fi
 
-# Constantes
-CAMINHO="/etc/apache2/sites-available/"
+# Constants
+APACHE_PATH="/etc/apache2/sites-available/"
 DOCROOT="/var/www/"
-DOMINIO="local"
+DOMAIN="local"
 
-echo "###############################################"
-echo "##  CRIACAO DE VIRTUALHOSTS DEBIAN E UBUNTU  ##"
-echo "###############################################"
+echo "####################################"
+echo "##  CREATING APACHE VIRTUALHOSTS  ##"
+echo "####################################"
 echo ""
 
-echo "Digite o nome do virtualhost (ex: boltbrasil):"
+echo "Type the virtualhost name (ie: testsite):"
 read vhost
 echo ""
 
-# Caso ele queira um dominio personalizado
-echo "Deseja criar um dominio personalizado?"
-echo "(ex: 'com.br' => '${vhost}.com.br', o padrao e '${vhost}.local') [enter para padrao]"
-read r_dominio
+echo "Do you wish to create a custom domain?"
+echo "(ie: 'com' => '${vhost}.com', the default is '${vhost}.local') [enter for default]"
+read r_domain
 echo ""
 
-if [[ "${r_dominio}" != "" ]]; then
-    DOMINIO=${r_dominio}
+if [[ "${r_domain}" != "" ]]; then
+    DOMAIN=${r_domain}
 fi
 
-# Verificando se o usuario apenas teclou 'enter'
-while [[ "${r_caminho}" != "s" ]] && [[ "${r_caminho}" != "n" ]] && [[ "${r_caminho}" == "" ]] && [[ "${r_caminho}" != "nao" ]] && [[ "${r_caminho}" != "sim" ]]; do
-    echo "Seus hosts estao no caminho padrao? [Digite 's' ou 'n']"
-    read r_caminho
+# If the user just typed 'enter'
+while [[ "${r_path}" != "y" ]] && [[ "${r_path}" != "n" ]] && [[ "${r_path}" == "" ]] && [[ "${r_path}" != "no" ]] && [[ "${r_path}" != "yes" ]]; do
+    echo "Your virtualhosts files are on the default path? [y/n]"
+    read r_path
     echo ""
 done
 
-# Se o caminho for diferente do padrao
-if [[ "${r_caminho}" == "n" ]] || [[ "${r_caminho}" == "nao" ]]; then
-    echo "Digite o caminho dos hosts (ex: '/home/nome-de-usuario/caminho/' SEMPRE com a barra no final):"
-    read caminho
+# If the virtualhosts path is differently than the default
+if [[ "${r_path}" == "n" ]] || [[ "${r_path}" == "no" ]]; then
+    echo "Type the path of the virtualhosts files (ie: '/home/username/path/' ALWAYS with the slash at the end):"
+    read vhost_path
     echo ""
-    CAMINHO=${caminho}
+    APACHE_PATH=${vhost_path}
 fi
 
-# Verificando se o dominio ja existe
-if ! grep "${dominio}" ${CAMINHO} >> /dev/null; then
-    echo "O virtualhost \"${vhost}.${DOMINIO}\" sera criado."
-    echo "Voce tem certeza que quer fazer isso? [s/n]"
+# Check if the domain already exists in the virtualhosts path
+if ! grep "${domain}" ${APACHE_PATH} >> /dev/null; then
+    echo "The virtualhost \"${vhost}.${DOMAIN}\" will be created."
+    echo "Are you sure? [y/n]"
     read q
     echo ""
 
     # Se tiver tudo certo, comeca a criar o virtualhost
-    if [[ "${q}" == "s" ]] || [[ "${q}" == "sim" ]]; then
+    if [[ "${q}" == "y" ]] || [[ "${q}" == "yes" ]]; then
         # Verificando se o usuario apenas teclou 'enter'
-        while [[ "${r_diretorio}" != "s" ]] && [[ "${r_diretorio}" != "n" ]] && [[ "${r_diretorio}" == "" ]] && [[ "${r_diretorio}" != "nao" ]] && [[ "${r_diretorio}" != "sim" ]]; do
-            echo "Voce deseja criar um diretorio para o seu dominio? [Digite 's' ou 'n']"
-            read r_diretorio
+        while [[ "${r_dir}" != "y" ]] && [[ "${r_dir}" != "n" ]] && [[ "${r_dir}" == "" ]] && [[ "${r_dir}" != "no" ]] && [[ "${r_dir}" != "yes" ]]; do
+            echo "Do you want to create a directory for your domain? [y/n]"
+            read r_dir
             echo ""
         done
 
         # Se o usuario deseja criar o diretorio
-        if [[ "${r_diretorio}" == "s" ]] || [[ "${r_diretorio}" == "sim" ]]; then
-            echo "Qual o nome do diretorio?"
+        if [[ "${r_dir}" == "y" ]] || [[ "${r_dir}" == "yes" ]]; then
+            echo "Wich is the name of the directory?"
             read dir
             echo ""
 
             while [[ "${dir}" == "" ]]; do
-                echo "Voce deve especificar o nome do diretorio!"
-                echo "Qual o nome do diretorio?"
+                echo "You must specify the directory name!"
+                echo "Wich is the directory name?"
                 read dir
                 echo ""
             done
-            
-            # Verificando se o DocumentRoot do Apache nao esta no padrao
-            echo "Qual o DocumentRoot do Apache?"
-            echo "(ex: '/home/www/', '/home/htdocs/' SEMPRE com a barra no final) [enter para padrao]"
+
+            # Verificando se o DocumentRoot do Apache no esta no padrao
+            echo "Wich is the path of the Apache DocumentRoot?"
+            echo "(ie: '/home/www/' => default, '/home/htdocs/' ALWAYS with the slash at the end) [enter for default]"
             read r_docroot
             echo ""
 
@@ -84,52 +83,51 @@ if ! grep "${dominio}" ${CAMINHO} >> /dev/null; then
                 DOCROOT=${r_docroot}
             fi
 
-            # Verificando se o diretorio ja existe, se nao, cria o diretorio,
+            # Verificando se o diretorio ja existe, se no, cria o diretorio,
             # seta permissoes de escrita pois foi criado como root, e cria o index.html
             # com uma mensagem de boas vindas
             if [[ ! -e ${DOCROOT}${dir} ]]; then
                 mkdir ${DOCROOT}${dir}
                 chmod 777 -R ${DOCROOT}${dir}
-                
+
                 touch ${DOCROOT}${dir}/index.html
                 chmod 777 ${DOCROOT}${dir}/index.html
-                
-                echo "OLA MUNDO! <br /><br />" > $DOCROOT$dir/index.html
-                echo "Este e o virtualhost '${vhost}.${DOMINIO}'!" >> ${DOCROOT}${dir}/index.html
+
+                echo "HELLO WORLD! <br /><br />" > $DOCROOT$dir/index.html
+                echo "This is the virtualhost created: '${vhost}.${DOMAIN}'!" >> ${DOCROOT}${dir}/index.html
             fi
         fi
 
         echo "
-### ${vhost}.${DOMINIO}
+### ${vhost}.${DOMAIN}
 <VirtualHost *:80>
-    ServerName ${vhost}.${DOMINIO}
+    ServerName ${vhost}.${DOMAIN}
     DocumentRoot ${DOCROOT}${dir}
     SetEnv APPLICATION_ENV development
-</VirtualHost>" >> ${CAMINHO}${vhost}.conf
+</VirtualHost>" >> ${APACHE_PATH}${vhost}.conf
 
         # Caso o DocumentRoot for o padrao do Ubuntu/Debian, adiciona o site com o comando 'a2ensite'
-        if [[ "${r_caminho}" == "s" ]] || [[ "${r_caminho}" == "sim" ]]; then
+        if [[ "${r_path}" == "y" ]] || [[ "${r_path}" == "yes" ]]; then
             a2ensite ${vhost}.conf
         fi
 
-        echo "Testando a configuracao."
+        echo "Checking the configuration."
         apache2ctl configtest
         echo ""
 
         # Adicionando entrada no hosts
-        echo "127.0.0.1 ${vhost}.${DOMINIO}" >> /etc/hosts
+        echo "127.0.0.1 ${vhost}.${DOMAIN}" >> /etc/hosts
 
-        while [[ "${r_reiniciar}" != "s" ]] && [[ "${r_reiniciar}" != "n" ]] && [[ "${r_reiniciar}" == "" ]] && [[ "${r_reiniciar}" != "nao" ]] && [[ "${r_reiniciar}" != "sim" ]]; do
-            echo "(Digite 's' ou 'n')"
-            echo "Voce gostaria de reiniciar o Apache? [Digite 's' ou 'n']"
-            read r_reiniciar
+        while [[ "${r_reload}" != "y" ]] && [[ "${r_reload}" != "n" ]] && [[ "${r_reload}" == "" ]] && [[ "${r_reload}" != "no" ]] && [[ "${r_reload}" != "yes" ]]; do
+            echo "Do hyou want to reload the Apache? [y/n]"
+            read r_reload
             echo ""
         done
-            
-        if [[ "${r_reiniciar}" == "sim" ]] || [[ "${r_reiniciar}" == "s" ]]; then
+
+        if [[ "${r_reload}" == "yes" ]] || [[ "${r_reload}" == "y" ]]; then
             apache2ctl restart
         fi
     fi
 else
-    echo "O virtualhost '${vhost}.${DOMINIO}' ja existe"
+    echo "The '${vhost}.${DOMAIN}' virtualhost already exists!"
 fi
